ce8f7eea3299dd96ecdd09796a09e482c193338e
edgarizagni31 <ochoaedgar3108@gmail.com>
2023-12-04 16:09:36 -0500

    remove subdomain configuration

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ dotenv.config();
 
 app.use(coors());
 app.use(morgan('dev'));
+app.set('subdomain offset', 1);
 app.set('port', process.env.PORT || '8080');
 app.use(express.json());
 app.use('/api/v1', router);
