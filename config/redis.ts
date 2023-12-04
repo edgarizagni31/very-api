45886472afd3e7eb23dc74ac81d47f0d4bfddbf4
edgarizagni31 <ochoaedgar3108@gmail.com>
2023-12-04 16:10:01 -0500

    configure redis url

--- config/redis.ts
+++ config/redis.ts
@@ -1,7 +1,14 @@
 import { createClient } from "redis";
+import * as dotenv from "dotenv";
+
+
+dotenv.config();
+
 
 export const redisClient = async () => {
-  return await createClient()
+  return await createClient({
+    url: process.env.REDIS_URL
+  })
     .on("error", (err) => console.log("Redis Client Error", err))
     .connect();
 };
