f2c05252118180dc027d95501b08c40e5812013e
edgarizagni31 <ochoaedgar3108@gmail.com>
2023-12-07 11:25:14 -0500

    add route to save orders and modify route names

--- routes.ts
+++ routes.ts
@@ -5,13 +5,15 @@ import { getProducts } from './controllers/product/getProducts';
 import { getProduct } from './controllers/product/getProduct';
 import { getCategories } from './controllers/category/getCategories';
 import { getStores } from './controllers/store/getStores';
+import { saveOrder } from './controllers/order/saveOrder';
 
 const router = Router();
 
-router.get('/', getStores);
-router.get('/:storeName', searchStore, getStore);
-router.get('/:storeName/productos', searchStore, getProducts);
-router.get('/:storeName/productos/:productId', searchStore, getProduct);
-router.get('/:storeName/categorias', searchStore, getCategories);
+router.get('/tiendas', getStores);
+router.get('/tiendas/:storeName', searchStore, getStore);
+router.get('/tiendas/:storeName/productos', searchStore, getProducts);
+router.get('/tiendas/:storeName/productos/:productId', searchStore, getProduct);
+router.get('/tiendas/:storeName/categorias', searchStore, getCategories);
+router.post('/tiendas/:storeName/order', searchStore, saveOrder);
 
 export default router;
